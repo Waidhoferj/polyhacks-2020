2d3db48785cfd90c1576328b43aa231b4824d8df
Waidhoferj <jwaidhof@calpoly.edu>
2020-02-08 15:36:58 -0800

    config

---
+++ src/modules/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/analytics";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB2hk1ykIc1D1pP1n2xRle_ZGHG7MVdhE4",
+  authDomain: "polyhacks-2020.firebaseapp.com",
+  databaseURL: "https://polyhacks-2020.firebaseio.com",
+  projectId: "polyhacks-2020",
+  storageBucket: "polyhacks-2020.appspot.com",
+  messagingSenderId: "505806295931",
+  appId: "1:505806295931:web:f5673304dbb6f91ff923de",
+  measurementId: "G-M6JEGW1960"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
