020f8ec40d9aa17966b0f83984bbb28afaa090d4
Waidhoferj <jwaidhof@calpoly.edu>
2020-02-08 15:56:12 -0800

    Added basic format to components

---
+++ src/components/MyRequests.vue
@@ -0,0 +1,21 @@
+<template> </template>
+
+<script>
+export default {
+  data() {
+    return {
+      //Put variables in this object
+      //Eg username: "Jason",
+    };
+  },
+  computed: {
+    // write functions that calculate a value
+    //Eg fullname: () => this.firstname + " " + this.lastname
+  },
+  methods: {
+    //All functions
+  }
+};
+</script>
+
+<style></style>
