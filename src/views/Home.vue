ca6079c096fb6e66ba33d8babc45e1a8161e9a17
jasonjtran <jtran200@calpoly.edu>
2020-02-09 07:27:42 -0800

    Removed comments

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,41 +21,6 @@ export default {
   data: () => {
     return {
       bounties: []
-
-      // bounties: [
-      //   {
-      //     title: "Avila Beach",
-      //     points: 200,
-      //     collected: 439,
-      //     company: "Cisco",
-      //     quantity: 500,
-      //     timeContraint: 1581226441791,
-      //     description:
-      //       "Take pictures of the sand at Avila Beach between the hours of 8:00 AM and 8:00 PM",
-      //     featured: true
-      //   },
-      //   {
-      //     title: "Dog pictures",
-      //     points: 200,
-      //     collected: 32,
-      //     company: "Activata",
-      //     quantity: 100,
-      //     timeContraint: 1581226441791,
-      //     description: "Collect them all",
-      //     featured: false
-      //   },
-      //   {
-      //     title: "Pictures of Fire Hydrants",
-      //     points: 500,
-      //     collected: 69,
-      //     company: "Knowmada",
-      //     quantity: 100,
-      //     timeContraint: 1581226441791,
-      //     description:
-      //       "Collect images of fire hydrants located around the downtown area",
-      //     featured: false
-      //   }
-      // ]
     };
   },
   mounted() {
