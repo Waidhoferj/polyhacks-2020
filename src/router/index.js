473c2751e94d064dba3e539a68e9ae3ec9161e96
Waidhoferj <jwaidhof@calpoly.edu>
2020-02-08 18:01:15 -0800

    added sign in and login route

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,29 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Login from "../views/Home.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/sign-up",
+    name: "SignUp",
+    component: () => import("../views/SignUp.vue")
+  },
   {
     path: "/home",
     name: "Home",
     component: Home
   },
   {
-    path: "/home",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
     path: "*",
-    name: "Home",
+    name: "Default",
     component: Login
   }
 ];
