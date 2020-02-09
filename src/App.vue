7ae7ae56f6949164d99498370f4e028f28fbeb1e
Alexander Rodgers <alexedrodgers@gmail.com>
2020-02-08 20:08:09 -0800

    Added sidebar

--- src/App.vue
+++ src/App.vue
@@ -6,55 +6,125 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-btn text icon
+          dark
+          @click.stop="drawer = !drawer"
+        >
+          <v-icon>menu</v-icon>
+        </v-btn>
+        <router-link to="/">
+          <v-btn text>Data Daddies</v-btn>
+        </router-link>
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
+      <v-container>
+      <v-sheet
+      height="100vh"
+      class="overflow-hidden"
+      style="position: relative;"
+    >
+    <v-container class="fill-height">
+      <v-row
+        align="center"
+        justify="center"
+      >
+      </v-row>
+    </v-container>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <v-list dense>
+
+        <v-list-item
+          v-for="item in account"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-sheet>
+  </v-container>
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
+    drawer: false,
+    items: [
+          { title: 'Home', icon: 'home' },
+          { title: 'Create Bounty', icon: 'create' },
+          { title: 'Current Bounties', icon: 'today' },
+          { title: 'Leaderboard', icon: 'poll' },
+        ],
+    account: [
+        { title: 'My Account', icon: 'account_circle' },
+        { title: 'Sign Out', icon: 'cloud_off'}
+    ]
     //
   }),
 };
 </script>
+
+<style scoped>
+.container {
+  max-width: inherit;
+  padding: 0px;
+}
+
+.registration {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+}
+</style>
